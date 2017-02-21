94beb5e7473715535cf8b99fb46ec89794c948ec
陈少凡 <394501487@qq.com>
2017-02-21 17:45:18 +0800

    修复绑定事件异常

--- index.js
+++ index.js
@@ -131,7 +131,9 @@ seeditForm.prototype.format = function(options){
 seeditForm.prototype.init = function(options){
 	this.format(options);
 	this.initHtml();
-	document.querySelector('body').innerHTML = document.querySelector('body').innerHTML + box;
+	$("body").append(box);
+	// 严重异常，以下写法会破坏所有之前的绑定事件
+	// document.querySelector('body').innerHTML = document.querySelector('body').innerHTML + box;
 	this.valid();
 	this.event();
 	this.initDistrict();
